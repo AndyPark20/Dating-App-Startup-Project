4a59366465381ef62846ea4b865fef6672dcb3ee
Andy Park <apark1214@gmail.com>
2022-05-19 09:52:19 -0700

    creating mini server to by pass CORS

--- my-app/src/Components/LandingPage/CandidateInfo/index.js
+++ my-app/src/Components/LandingPage/CandidateInfo/index.js
@@ -1,36 +1,41 @@
 import React from "react";
-
+import { fetchCandidateApi } from "../../../functions/api";
 
 //Import CSS
-import './CandidateInfo.css';
-
-
+import "./CandidateInfo.css";
 
 export const RenderCandidate = ({ candidateApiData }) => {
-
   //Function to fetch Api to its this for that business idea
-  const fetchBizIdea= async()=>{
-      const bizIdeaData = await fetch("http://itsthisforthat.com/api.php?call=myfunc");
+  const test =()=>{
+  const fetchBizIdea = async () => {
+    if (fetchCandidateApi) {
+      const bizIdeaData = await fetch("http://itsthisforthat.com/api.php?json");
       const bizIdeaDataResult = await bizIdeaData.json();
-      console.log('biz idea', bizIdeaDataResult);
+      console.log("biz idea", bizIdeaDataResult);
+    }
+  };
+  fetchBizIdea();
   }
 
 
   //use Map function to loop thru the userApi object
   const renderCandidates = () => {
-
     //First render will be an empty array. Run when data has been retrieved
     if (candidateApiData.results) {
+
       const invdividualCandidates = candidateApiData.results.map((values, index) => {
           return (
-              <div className="candidate-info">
-                <img src={values.picture.large} alt={`${values.name.first} ${values.name.last}`}/>
-                <div className="candidate-name">
-                  <p className="first">{values.name.first}</p>
-                  <p className="last">{values.name.last}</p>
-                  <p className="biz-idea">{fetchBizIdea()}</p>
-                  </div>
+            <div className="candidate-info">
+              <img
+                src={values.picture.large}
+                alt={`${values.name.first} ${values.name.last}`}
+              />
+              <div className="candidate-name">
+                <p className="first">{values.name.first}</p>
+                <p className="last">{values.name.last}</p>
+                <p className="biz-idea"></p>
               </div>
+            </div>
           );
         }
       );
@@ -38,5 +43,11 @@ export const RenderCandidate = ({ candidateApiData }) => {
     }
   };
 
-  return <div className="candidate-row-section">{renderCandidates()}</div>;
+  return (
+  <div className="candidate-row-section">
+    {renderCandidates()}
+    <div>
+      {test()}
+    </div>
+  </div>);
 };
---
+++ my-app/src/Server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fetch = require('fetch');
+const json = require('express-json');
+
+console.log(app);
