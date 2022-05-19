d33ed5e1c3d3ea8aa515209f547600f0bcd199b1
Andy Park <apark1214@gmail.com>
2022-05-18 20:51:24 -0700

    working on rendering user Data

---
+++ my-app/src/Components/LandingPage/CandidateInfo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export const RenderCandidate =(userApiData)=>{
+
+  //use Map function to loop thru the userApi object
+
+  return (
+    <div>
+      {console.log(userApi)}
+    </div>
+  )
+}
--- my-app/src/Components/LandingPage/index.js
+++ my-app/src/Components/LandingPage/index.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+//Import child components
+import { RenderCandidate } from "./CandidateInfo";
+
+
+
 
 export const LandingPage = () => {
+  //Store object retrieved from Api fetch in order to pass down as props to child components
+  const [userApi, updateUserApi] = useState({});
 
-  const randomUserApi =()=>{
-    //function to Fetch API from Random User API
-    const getRandomUserApi= async () => {
+  useEffect(() => {
+    const getRandomUserApi = async () => {
+
       try {
         const randomUserData = await fetch("https://randomuser.me/api/?results=15");
         const dataJson = await randomUserData.json();
-        console.log("dataJson", dataJson.results);
+        updateUserApi(dataJson);
+
       } catch (err) {
         console.error(err);
       }
     };
     getRandomUserApi();
-  }
+  },[]);
 
-  return <div>{randomUserApi()}</div>;
+  return (
+    <div>
+      <RenderCandidate userApiData={userApi} />
+    </div>
+  );
 };
