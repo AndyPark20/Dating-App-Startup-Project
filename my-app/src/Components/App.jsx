5374e59a73a7dccf8dc3fbc5058942c87301de2c
Andy Park <apark1214@gmail.com>
2022-05-18 20:35:32 -0700

    rendering candidate information

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+//Import CSS File
+import "./App.css";
+
 //Import Components
 import { LandingPage } from './LandingPage/';
-
+import { VerticalMenu } from './VerticalMenu';
 
 
 export const App =()=>{
-  return(
-    <div>
-      <LandingPage/>
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col main-section">
+          <div className="vertical-menu">
+            <VerticalMenu />
+          </div>
+          <LandingPage />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- my-app/src/Components/VerticalMenu/index.js
+++ my-app/src/Components/VerticalMenu/index.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+
+
+export const VerticalMenu =()=>{
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col">
+            <h4>Hello</h4>
+            <h4>Goodbye</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
