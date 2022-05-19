75f658c6cf128804077d86aa718812c625ffb59a
Andy Park <apark1214@gmail.com>
2022-05-19 10:17:40 -0700

    Backend server for bypassing CORS working

--- my-app/src/Server/server.js
+++ my-app/src/Server/server.js
@@ -1,7 +1,29 @@
+
+require('dotenv/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const fetch = require('fetch');
-const json = require('express-json');
+const fetch = require('node-fetch');
+const jsonMiddleWare = require('express-json');
 
-console.log(app);
+
+app.use(cors());
+app.use(jsonMiddleWare());
+
+
+async function test (req,res){
+  try{
+    const data = await fetch("http://itsthisforthat.com/api.php?json");
+    const dataResult = await data.json();
+    console.log(dataResult)
+  }catch(err){
+    console.log(err)
+  }
+}
+
+
+test();
+
+app.listen(process.env.PORT,()=>{
+  console.log(`express server listening on port ${process.env.PORT}`);
+})
