d6abc574267407d959453c2f638aac5844dfc657
Andy Park <apark1214@gmail.com>
2022-05-18 23:07:10 -0700

    adding display x amount of candidates per page functionality

--- my-app/src/Components/Footer/index.js
+++ my-app/src/Components/Footer/index.js
@@ -6,13 +6,19 @@ import './Footer.css';
 
 
 export const Footer =()=>{
-  return(
+  return (
     <div className="footer-container">
-        <div className="row">
-          <div className="col page-per-view">
-            <p>Display</p>
-          </div>
+      <div className="row">
+        <div className="col page-per-view">
+          <p>Display</p>
+          <select>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+          <p>Candidates per page</p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
