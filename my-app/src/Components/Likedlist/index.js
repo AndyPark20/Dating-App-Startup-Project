6bb9652b59b4cae54a9df9ea9f7db6f4c16a9b88
AndyPark20 <apark1214@gmail.com>
2022-05-24 04:43:44 +0000

    in progress

--- my-app/src/Components/Likedlist/index.js
+++ my-app/src/Components/Likedlist/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+//Import components
+import {InfoHeader} from '../LandingPage/InfoHeader';
 
-export const LikedList =()=>{
+export const LikedList =({})=>{
   return(
     <div>
-      <h1>Hello</h1>
+      <InfoHeader/>
     </div>
   )
 }
